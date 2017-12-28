06f02a19df2bd8ea12897f646b75b8409d58ee5d
David Gethings <dgethings@juniper.net>
2017-12-28 10:14:39 +0000

    flip files to make it more reusable

---
+++ cli.py
@@ -0,0 +1,81 @@
+#!/usr/bin/python
+# <*******************
+# 
+# Copyright 2018 Juniper Networks, Inc. All rights reserved.
+# Licensed under the Juniper Networks Script Software License (the "License").
+# You may not use this script file except in compliance with the License, which is located at
+# http://www.juniper.net/support/legal/scriptlicense/
+# Unless required by applicable law or otherwise agreed to in writing by the parties, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# 
+# *******************>
+"""
+    Wrapper script for all NITA commands
+"""
+import sys
+import os
+
+def nested_keys(dictionary, path=None):
+    """
+    Function that returns all nested keys in a dictionary.
+    """
+    if path is None:
+        path = []
+    for k,v in dictionary.items():
+        newpath = path + [k]
+        if isinstance(v, dict):
+            for u in nested_keys(v, newpath):
+                yield u
+        else:
+            yield newpath, v
+
+def print_nested_keys(dictionary):
+    """
+    Function that prints a set of all nested keys (from root to leaf) 
+    in a dictionary with its correspondent value.
+    """
+    for root2leaf, value in nested_keys(dictionary):
+        # Not print the license when in help
+        if 'license' not in root2leaf:
+            print '   ' + commands.KEY_SEPARATOR.join(root2leaf) + ' => ' + value
+
+def cli2command(cli, translator):    
+    for k in cli: 
+        translator = translator[str(k)]
+    return translator
+
+def main():
+    # print 'Number of arguments:', len(sys.argv), 'arguments.'
+    # print 'Argument List:', str(sys.argv)
+    if 'help' in sys.argv:
+        print ''
+        print_nested_keys(commands.HELP)
+        print ''
+        sys.exit()
+
+    # Remove /usr/local/bin/ from first argument (/usr/local/bin/nita)
+    root = sys.argv[0].split('/')[-1]
+    cli = sys.argv[1:]
+    cli.insert(0, root)
+
+    try:
+        command = cli2command(cli, commands.COMMANDS)
+        # If % vars in command
+        if '%' in command:
+                
+            print ''
+            print '  >>>> command: ', command % commands.PROJECT_PATH
+            print ''
+            os.system(command % commands.PROJECT_PATH)
+
+        else:
+
+            print ''
+            print '  >>>> command: ', command
+            print ''
+            os.system(command)
+
+    except KeyError as ke:
+        print ''
+        print('"' + ke.message + '" key does not exist at COMMANDS dictionary')
+        print ''
