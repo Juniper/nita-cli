f6c840fdb8ff2520f816e43dadadb28a5006d34f
David Gethings <dgethings@juniper.net>
2017-12-28 11:50:28 +0000

    move KeyError expection handling to cli2command()

--- cli.py
+++ cli.py
@@ -24,7 +24,7 @@ def nested_keys(dictionary, path=None):
     """
     if path is None:
         path = []
-    for k,v in dictionary.items():
+    for k, v in dictionary.items():
         newpath = path + [k]
         if isinstance(v, dict):
             for u in nested_keys(v, newpath):
@@ -34,16 +34,26 @@ def nested_keys(dictionary, path=None):
 
 def print_nested_keys(dictionary):
     """
-    Function that prints a set of all nested keys (from root to leaf) 
+    Function that prints a set of all nested keys (from root to leaf)
     in a dictionary with its correspondent value.
     """
     dictionary.pop('license', None)
     for root2leaf, value in nested_keys(dictionary):
         print '   ' + KEY_SEPARATOR.join(root2leaf) + ' => ' + value
 
-def cli2command(cli, translator):    
-    for k in cli: 
-        translator = translator[str(k)]
+def cli2command(cli, translator):
+    """
+    Function resolves cli command given to command to exec
+    """
+    try:
+        for k in cli:
+            translator = translator[str(k)]
+    except KeyError as err:
+        print ''
+        print "'{}' key does not exist at COMMANDS dictionary".format(err.message)
+        print ''
+        sys.exit(1)
+
     return translator
 
 def main(commands, help):
@@ -60,24 +70,18 @@ def main(commands, help):
     cli = sys.argv[1:]
     cli.insert(0, root)
 
-    try:
-        command = cli2command(cli, commands)
-        # If % vars in command
-        if '%' in command:
-                
-            print ''
-            print '  >>>> command: ', command % commands.PROJECT_PATH
-            print ''
-            os.system(command % commands.PROJECT_PATH)
-
-        else:
-
-            print ''
-            print '  >>>> command: ', command
-            print ''
-            os.system(command)
-
-    except KeyError as ke:
+    command = cli2command(cli, commands)
+    # If % vars in command
+    if '%' in command:
+            
         print ''
-        print('"' + ke.message + '" key does not exist at COMMANDS dictionary')
+        print '  >>>> command: ', command % commands.PROJECT_PATH
         print ''
+        os.system(command % commands.PROJECT_PATH)
+
+    else:
+
+        print ''
+        print '  >>>> command: ', command
+        print ''
+        os.system(command)
