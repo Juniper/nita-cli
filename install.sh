28e1bae65e447d7ddf66a8d21342f95388f66920
Jose Miguel Izquierdo <jizquierdo@juniper.net>
2018-01-09 11:02:04 +0100

    Add LICENSE to script

--- install.sh
+++ install.sh
@@ -1,4 +1,15 @@
-#!/bin/
+#!/bin/bash
+# <*******************
+#
+# Copyright 2018 Juniper Networks, Inc. All rights reserved.
+# Licensed under the Juniper Networks Script Software License (the "License").
+# You may not use this script file except in compliance with the License, which is located at
+# http://www.juniper.net/support/legal/scriptlicense/
+# Unless required by applicable law or otherwise agreed to in writing by the parties, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#
+# *******************>
 echo ""
 echo " >>>> Copying cli.py & nita to /usr/local/bin/"
 cp cli.py nita /usr/local/bin/
